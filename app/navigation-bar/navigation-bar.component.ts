c140cae512323d3d8740567f70580205cff940d0
anujparikh <anujparikh1@gmail.com>
2016-07-11 10:26:17 -0500

    initial commit

---
+++ app/navigation-bar/navigation-bar.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'nav-bar',
+    templateUrl: 'app/navigation-bar/navigation-bar.component.html',
+    styleUrls: ['app/navigation-bar/navigation-bar.component.css']
+})
+
+export class NavigationBarComponent {
+
+}
