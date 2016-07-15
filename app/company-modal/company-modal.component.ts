1b1f94021caa3aab2aaf880b57c84a218550fc14
anujparikh <anujparikh1@gmail.com>
2016-07-14 21:34:06 -0500

    company modal component

---
+++ app/company-modal/company-modal.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'company-modal',
+    templateUrl: 'app/company-modal/company-modal.component.html',
+    styleUrls: ['app/company-modal/company-modal.component.css']
+})
+
+export class CompanyModalComponent {
+
+}
--- app/project-section/project-section.component.ts
+++ app/project-section/project-section.component.ts
@@ -1,9 +1,13 @@
-import {Component} from "@angular/core";
+import {Component, ViewEncapsulation} from "@angular/core";
+
+import {CompanyModalComponent} from "../company-modal/company-modal.component";
 
 @Component({
     selector: 'project-section',
     templateUrl: 'app/project-section/project-section.component.html',
-    styleUrls: ['app/project-section/project-section.component.css']
+    styleUrls: ['app/project-section/project-section.component.css'],
+    directives: [CompanyModalComponent],
+    encapsulation: ViewEncapsulation.None
 })
 
 export class ProjectSectionComponent {
